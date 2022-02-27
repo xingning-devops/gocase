7f33b3cc5cce9b976026f126dad9a7502eddda78
xingning <15010741839@163.com>
2022-02-27 13:08:15 +0800

    httpserver添加优雅终止

--- 2.2/httpserver/main.go
+++ 2.2/httpserver/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"httpserver/model"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -11,9 +15,40 @@ func main() {
 	http.HandleFunc("/", model.Index)
 	http.HandleFunc("/healthz", model.Healthz)
 
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Fatal(err)
+	s := &http.Server{
+		Addr:           ":80",
+		Handler:        http.DefaultServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
+
+	// make sure idle connections returned
+	processed := make(chan struct{})
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		if err := s.Shutdown(ctx); nil != err {
+			log.Fatalf("server shutdown failed, err: %v\n", err)
+		}
+
+		time.Sleep(time.Second * 10)
+		log.Println("server gracefully shutdown")
+
+		close(processed)
+	}()
+
+	// serve
+	err := s.ListenAndServe()
+	if http.ErrServerClosed != err {
+		log.Fatalf("server not gracefully shutdown, err :%v\n", err)
+	}
+
+	// waiting for goroutine above processed
+	<-processed
 
 }
